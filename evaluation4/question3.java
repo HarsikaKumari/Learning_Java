c886d1931eb27a2631050282ffb49f28d14a1143
Harsika Kumari <kumariharsika8@gmail.com>
2024-04-26 11:58:14 +0530

    make it a proper code for custom exception

--- evaluation4/question3.java
+++ evaluation4/question3.java
@@ -1,10 +1,30 @@
+// package evaluation4;
+
+// public class question3 {
+//     public static void main(String[] args) {
+//         try {
+//             throw new Exception("This is a custom exception!");
+//         } catch (Exception e) {
+//             System.out.println("Custom exception caught: " + e.getMessage());
+//         }
+//     }
+// }
+
 package evaluation4;
+
+// Define a user-defined exception
+class CustomException extends Exception {
+    public CustomException(String message) {
+        super(message);
+    }
+}
 
 public class question3 {
     public static void main(String[] args) {
         try {
-            throw new Exception("This is a custom exception!");
-        } catch (Exception e) {
+            // Throw the user-defined exception
+            throw new CustomException("This is a custom exception!");
+        } catch (CustomException e) {
             System.out.println("Custom exception caught: " + e.getMessage());
         }
     }
