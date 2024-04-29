5f3a67533e10557a5d65a277dd85e9fee2b9cd92
Harsika Kumari <kumariharsika8@gmail.com>
2024-04-29 11:44:55 +0530

    made some changes

--- abstraction/Animal.java
+++ abstraction/Animal.java
@@ -1,23 +1,19 @@
-//abstract class
-interface Animal {
-    // abstract method (does not have a body)
+interface animalFeat {
     public void sleep(); // interface method (does not have a body)
 }
 
 // pig "implements" the animal interface
-class pig implements Animal {
+class pig implements animalFeat {
     public void animalSound() {
-        // the body of animal sound is provided here
         System.out.println("The pig says: Wee Wee");
     }
-
+    
     public void sleep() {
-        // the body of sleep() is provided here
         System.out.println("Zzzz");
     }
 }
 
-class Animals {
+class Animal {
     public static void main(String[] args) {
         pig myPig = new pig(); // Create a pig object
         myPig.animalSound();
